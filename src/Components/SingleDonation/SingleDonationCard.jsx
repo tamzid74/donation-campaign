ca486b8147c1a45178f70d4f7c9b8d5bd6387cd8
tamzid74 <tamzidal74@gmail.com>
2023-09-25 18:52:04 +0600

    sweet alert added in donation card, when you click donate button twice sweet alert warn you that you already donated

--- src/Components/SingleDonation/SingleDonationCard.jsx
+++ src/Components/SingleDonation/SingleDonationCard.jsx
@@ -1,7 +1,41 @@
+import swal from "sweetalert";
+
 /* eslint-disable react/prop-types */
 const SingleDonationCard = ({ singleDonation }) => {
-  const { picture, title, text_button_bg_color, description, donation } =
+  const { id, picture, title, text_button_bg_color, description, donation } =
     singleDonation || {};
+
+
+    const handleToAdd =() =>{
+        const addedDonationsArray = [];
+
+        const donationItems = JSON.parse(localStorage.getItem('donations'));
+
+        if(!donationItems){
+            addedDonationsArray.push(singleDonation);
+            localStorage.setItem("donations",JSON.stringify(addedDonationsArray));
+            swal("Good job!", "Donation Done", "success");
+        }
+        else{
+            const isExits = donationItems.find(singleDonation=> singleDonation.id === id);
+
+            if(!isExits){
+
+                addedDonationsArray.push(...donationItems,singleDonation);
+                localStorage.setItem('donations',JSON.stringify(addedDonationsArray));
+                swal("Good job!", "Donation Done", "success");
+            }else{
+                swal("Error!", "Already donated!", "error");
+            }
+        }
+    }
+
+
+
+
+
+
+
   return (
     
       <div className="w-full space-y-6 p-5">
@@ -11,7 +45,7 @@ const SingleDonationCard = ({ singleDonation }) => {
           />
           {/* <div className="absolute w-full bottom-0 bg-black opacity-50 p-14 ">
           </div> */}
-          <button style={{background:text_button_bg_color}} className=" absolute bottom-9 ml-9 rounded-lg text-sm lg:text-xl font-semibold text-white px-4 py-2">Donate ${donation}</button>
+          <button onClick={handleToAdd} style={{background:text_button_bg_color}} className=" absolute bottom-9 ml-9 rounded-lg text-sm lg:text-xl font-semibold text-white px-4 py-2">Donate ${donation}</button>
           <h2 className="text-4xl font-bold">{title}</h2>
           <p>{description}</p>
           
